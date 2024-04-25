60c6d606b13e010bc7156ec6ec04b0414ae387d9
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-04-25 18:31:11 +0200

    add operations

--- cropgraph/column_operations.go
+++ cropgraph/column_operations.go
@@ -20,6 +20,7 @@ func HandleColumnViewOperation(operationDefinition OperationDefinition, columnVa
 		newColumnValues = columnValues[0]
 	}
 	// TODO: Implement multiplication for each element in the result column
+	newColumnValues = MultiplyColumnValues(newColumnValues, operationDefinition.Multiply)
 
 	return newColumnValues
 }
@@ -48,6 +49,12 @@ func diffOperation(columnValues [][]interface{}) []interface{} {
 	// as a result, you should have one new slice of daily differences between the values of each column
 	// e.g. columnValues[0][0] - columnValues[1][0] = newColumnValues[0]
 	newColumnValues := make([]interface{}, len(columnValues[0])) // create a new slice of columnValues []interfaceParseFloat()
+	for j := 0; j < len(columnValues[0]); j++ {
+		newColumnValues[j] = AsFloat(columnValues[0][j])
+		for i := 1; i < len(columnValues); i++ {
+			newColumnValues[j] = newColumnValues[j].(float64) - AsFloat(columnValues[i][j])
+		}
+	}
 
 	return newColumnValues
 }
@@ -59,6 +66,15 @@ func avgOperation(columnValues [][]interface{}) []interface{} {
 	// as a result, you should have one new slice of daily averages between the values of each column
 	// the formula for the average is the sum of all values divided by the number of values
 	newColumnValues := make([]interface{}, len(columnValues[0])) // create a new slice of columnValues []interface{}
+	numColumnValues := float64(len(columnValues))
+
+	for j := 0; j < len(columnValues[0]); j++ {
+		sum := 0.0
+		for i := 0; i < len(columnValues); i++ {
+			sum = sum + AsFloat(columnValues[i][j])
+		}
+		newColumnValues[j] = sum / numColumnValues
+	}
 
 	return newColumnValues
 }
@@ -69,17 +85,45 @@ func dailyDifferenceOperation(columnValues []interface{}) []interface{} {
 	// write a loop to iterate over the columnValues
 	// and calculate the difference between two consecutive days into a new slice newColumnValues
 	// as a result, you should have one new slice of daily differences between the values of each column
-	// e.g. columnValues[0][0] - columnValues[0][1] = newColumnValues[0]
+	// e.g. columnValues[1] - columnValues[0] = newColumnValues[0]
 	// the first value of the newColumnValues should be 0, as there is no previous value to calculate the difference from
 	newColumnValues := make([]interface{}, len(columnValues)) // create a new slice of columnValues []interface{}
+
+	newColumnValues[0] = 0.0
+	for i := 1; i < len(columnValues); i++ {
+		newColumnValues[i] = AsFloat(columnValues[i]) - AsFloat(columnValues[i-1])
+	}
 
 	return newColumnValues
 }
 
 func AsFloat(value interface{}) float64 {
-	parsedValue, err := strconv.ParseFloat(value.(string), 64)
-	if err != nil {
-		panic(err)
+	// check if the value is a string and convert it to a float64
+	// if the value is already a float64, return it
+	// if the value is not a string or a float64, panic
+	if _, ok := value.(float64); ok {
+		return value.(float64)
 	}
-	return parsedValue
+
+	if _, ok := value.(string); ok {
+		parsedValue, err := strconv.ParseFloat(value.(string), 64)
+		if err != nil {
+			panic(err)
+		}
+		return parsedValue
+	}
+	panic("value is not a float64 or a string")
+}
+
+func MultiplyColumnValues(columnValues []interface{}, factor float64) []interface{} {
+	// check if factor is 0 and return the columnValues as they are
+	// if factor is not 0, multiply each value in the columnValues with the factor
+	if factor == 0 || factor == 1 {
+		return columnValues
+	}
+
+	for i := 0; i < len(columnValues); i++ {
+		columnValues[i] = AsFloat(columnValues[i]) * factor
+	}
+	return columnValues
 }
